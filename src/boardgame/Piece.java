afc38ccd832439e7d0dcfa12db446cd86e122c0c
Renan Boff Junior <renan.junior@hotmail.com>
2020-12-20 15:01:38 -0300

    Movimentos possíeis para a peça

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;			// foi colocada com 'protected', para não ficar visível na camada do jogo
 	private Board board;
@@ -12,5 +12,23 @@ public class Piece {
 
 	protected Board getBoard() {			// get do 'board'. O set foi apagado para não permitir que o tabuleiro seja alterado
 		return board;						// o getboard será 'protected', para ser acessado somente na camada do tabuleiro
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Não existe peça na posição de origem");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao existe movimentos possiveis para a peca escolhida");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
