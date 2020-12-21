1544f5b2b6728fef87e89d65a5bffb06de60d0a7
Renan Boff Junior <renan.junior@hotmail.com>
2020-12-21 12:53:07 -0300

    Printando as posições possíveis de movimento da peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {	// permite imprimir os movimentos possíveis da peça, a partir de uma posição de origem
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece preformChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
