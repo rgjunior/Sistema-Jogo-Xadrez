70cf579dde706ad73e4e3b25220ddd4698906a55
Renan Boff Junior <renan.junior@hotmail.com>
2020-12-28 18:36:55 -0300

    Movimento especial: roque

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,14 +1,20 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
+	
+	private ChessMatch chessMatch;
+	
 
-	public King(Board board, Color color) {
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 		}
 
 	@Override
@@ -20,12 +26,16 @@ public class King extends ChessPiece {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
 	}
-		
+	
+	private boolean testRookCastle(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
+	}
+	
 		
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
 		Position p = new Position(0, 0);
 		
 		// teste se o Rei poderá se mover para cima
@@ -76,7 +86,28 @@ public class King extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		return mat;
+		// movimento especial 'roque'
+		if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// roque pequeno
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+			if (testRookCastle(posT1)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					mat[position.getRow()][position.getColumn() + 2] = true;
+				}
+			}
+			// roque grande
+				Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+				if (testRookCastle(posT2)) {
+					Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+					Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+					Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+					if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+						mat[position.getRow()][position.getColumn() - 2] = true;
+					}
+				}
+			}
+			return mat;
 	}
-	
 }
